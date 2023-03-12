6e662000d6e49ca8f85f7c0f779abb28f0a19234
ranaintizar <intizaralirana2@gmail.com>
2023-03-12 17:38:29 +0500

    Create Personal Info Section

--- components/Profile.js
+++ components/Profile.js
@@ -1,110 +1,204 @@
-import React from "react";
-import { StyleSheet, View, Text, Image } from "react-native";
+import React, { useState, useRef } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  TouchableWithoutFeedback,
+  Keyboard,
+  Animated,
+  Modal,
+} from "react-native";
 import { MaterialIcons, Ionicons } from "@expo/vector-icons";
+import Button from "./Button";
+import PersonalInfo from "./PersonalInfo";
 
 const Profile = ({ theme }) => {
+  const [slideAnimation, setSlideAnimation] = useState(new Animated.Value(0));
+  const [isVisible, setIsVisible] = useState(true);
+
+  const handleShow = () => {
+    Animated.spring(slideAnimation, {
+      toValue: 1,
+      duration: 100,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const handleHide = () => {
+    Animated.spring(slideAnimation, {
+      toValue: 0,
+      duration: 100,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const slideUp = slideAnimation.interpolate({
+    inputRange: [0, 1],
+    outputRange: [300, 0],
+  });
+
   return (
-    <View
-      style={[
-        { flex: 1 },
-        theme === "light"
-          ? { backgroundColor: "#fff" }
-          : { backgroundColor: "#16161a" },
-      ]}
-    >
-      <View style={styles.userInfo}>
-        <Image
-          source={require("../assets/Profile-Pic.jpg")}
-          style={styles.img}
-        />
-        <View style={styles.nameContainer}>
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View
+        style={[
+          { flex: 1 },
+          theme === "light"
+            ? { backgroundColor: "#fff" }
+            : { backgroundColor: "#16161a" },
+        ]}
+      >
+        <View style={styles.userInfo}>
+          <Image
+            source={require("../assets/Profile-Pic.jpg")}
+            style={styles.img}
+          />
+          <View style={styles.nameContainer}>
+            <Text
+              style={[
+                styles.name,
+                theme === "light" ? { color: "#16161a" } : { color: "#fff" },
+              ]}
+            >
+              John Doe
+            </Text>
+            <Text
+              style={[
+                styles.username,
+                theme === "light" ? { color: "#16161a" } : { color: "#fff" },
+              ]}
+            >
+              @johndoe
+            </Text>
+          </View>
+        </View>
+        <View style={styles.options}>
+          <TouchableWithoutFeedback onPress={() => setIsVisible(true)}>
+            <View style={styles.option}>
+              <View style={styles.optionName}>
+                <Ionicons
+                  name="person-outline"
+                  size={24}
+                  color={theme === "light" ? "black" : "#fff"}
+                />
+                <Text
+                  style={[
+                    styles.optionTitle,
+                    theme === "light"
+                      ? { color: "#16161a" }
+                      : { color: "#fff" },
+                  ]}
+                >
+                  Personal Information
+                </Text>
+              </View>
+              <MaterialIcons
+                name="navigate-next"
+                size={26}
+                color={theme === "light" ? "black" : "#fff"}
+              />
+            </View>
+          </TouchableWithoutFeedback>
+          <TouchableWithoutFeedback onPress={() => console.log("Button")}>
+            <View style={styles.option}>
+              <View style={styles.optionName}>
+                <Ionicons
+                  name="ios-settings-outline"
+                  size={24}
+                  color={theme === "light" ? "black" : "#fff"}
+                />
+                <Text
+                  style={[
+                    styles.optionTitle,
+                    theme === "light"
+                      ? { color: "#16161a" }
+                      : { color: "#fff" },
+                  ]}
+                >
+                  Settings
+                </Text>
+              </View>
+              <MaterialIcons
+                name="navigate-next"
+                size={26}
+                color={theme === "light" ? "black" : "#fff"}
+              />
+            </View>
+          </TouchableWithoutFeedback>
+          <TouchableWithoutFeedback onPress={handleShow}>
+            <View style={styles.option}>
+              <View style={styles.optionName}>
+                <MaterialIcons
+                  name="logout"
+                  size={24}
+                  color={theme === "light" ? "black" : "#fff"}
+                />
+                <Text
+                  style={[
+                    styles.optionTitle,
+                    theme === "light"
+                      ? { color: "#16161a" }
+                      : { color: "#fff" },
+                  ]}
+                >
+                  Logout
+                </Text>
+              </View>
+              <MaterialIcons
+                name="navigate-next"
+                size={26}
+                color={theme === "light" ? "black" : "#fff"}
+              />
+            </View>
+          </TouchableWithoutFeedback>
+        </View>
+        <Animated.View
+          style={[
+            styles.confirmDialog,
+            {
+              position: "absolute",
+              bottom: 0,
+              left: "10%",
+              transform: [{ translateY: slideUp }],
+            },
+            theme === "light"
+              ? { backgroundColor: "#0009" }
+              : { backgroundColor: "#f1f2f3" },
+          ]}
+        >
           <Text
             style={[
-              styles.name,
-              theme === "light" ? { color: "#16161a" } : { color: "#72757e" },
+              styles.confirmTitle,
+              theme === "light" ? { color: "#fff" } : { color: "#72757e" },
             ]}
           >
-            John Doe
+            Log out?
           </Text>
-          <Text
-            style={[
-              styles.username,
-              theme === "light" ? { color: "#16161a" } : { color: "#72757e" },
-            ]}
-          >
-            @johndoe
-          </Text>
-        </View>
+          <View style={styles.divider}></View>
+          <Button
+            btnText="Yes"
+            background="#1e90ff"
+            fontSize={20}
+            width={250}
+            customClass={{ marginVertical: 5 }}
+          />
+          <View style={styles.divider}></View>
+          <Button
+            btnText="No"
+            borderWidth={2}
+            borderColor="#1e90ff"
+            color="#1e90ff"
+            fontSize={20}
+            width={250}
+            customClass={{ paddingVertical: 2 }}
+            onPress={handleHide}
+          />
+        </Animated.View>
+        <Modal visible={isVisible} statusBarTranslucent={true}>
+          <PersonalInfo theme={theme} showModal={setIsVisible} />
+        </Modal>
       </View>
-      <View style={styles.options}>
-        <View style={styles.option}>
-          <View style={styles.optionName}>
-            <Ionicons
-              name="person-outline"
-              size={24}
-              color={theme === "light" ? "black" : "#fff"}
-            />
-            <Text
-              style={[
-                styles.optionTitle,
-                theme === "light" ? { color: "#16161a" } : { color: "#fff" },
-              ]}
-            >
-              Personal Information
-            </Text>
-          </View>
-          <MaterialIcons
-            name="navigate-next"
-            size={26}
-            color={theme === "light" ? "black" : "#fff"}
-          />
-        </View>
-        <View style={styles.option}>
-          <View style={styles.optionName}>
-            <Ionicons
-              name="ios-settings-outline"
-              size={24}
-              color={theme === "light" ? "black" : "#fff"}
-            />
-            <Text
-              style={[
-                styles.optionTitle,
-                theme === "light" ? { color: "#16161a" } : { color: "#fff" },
-              ]}
-            >
-              Settings
-            </Text>
-          </View>
-          <MaterialIcons
-            name="navigate-next"
-            size={26}
-            color={theme === "light" ? "black" : "#fff"}
-          />
-        </View>
-        <View style={styles.option}>
-          <View style={styles.optionName}>
-            <MaterialIcons
-              name="logout"
-              size={24}
-              color={theme === "light" ? "black" : "#fff"}
-            />
-            <Text
-              style={[
-                styles.optionTitle,
-                theme === "light" ? { color: "#16161a" } : { color: "#fff" },
-              ]}
-            >
-              Logout
-            </Text>
-          </View>
-          <MaterialIcons
-            name="navigate-next"
-            size={26}
-            color={theme === "light" ? "black" : "#fff"}
-          />
-        </View>
-      </View>
-    </View>
+    </TouchableWithoutFeedback>
   );
 };
 
@@ -127,7 +221,7 @@ const styles = StyleSheet.create({
   username: {
     fontSize: 18,
   },
-  options: { flex: 0.6, paddingTop: 30 },
+  options: { flex: 0.6, paddingTop: 10 },
   option: {
     paddingVertical: 10,
     paddingHorizontal: 20,
@@ -141,6 +235,25 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   optionTitle: { fontSize: 22 },
+
+  confirmDialog: {
+    position: "absolute",
+    bottom: 0,
+    width: "80%",
+    height: 200,
+    borderRadius: 20,
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    backgroundColor: "#0009",
+  },
+  confirmTitle: {
+    fontSize: 20,
+  },
+  divider: {
+    width: "80%",
+    height: 2,
+    backgroundColor: "#72757e",
+  },
 });
 
 export default Profile;
